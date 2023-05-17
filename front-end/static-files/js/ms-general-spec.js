d06a72d6977daac2d6d068f27d0becbaf366b08e
Magdalena Bueno Pedrera <mmbp0003@red.ujaen.es>
2023-05-17 22:40:57 +0200

    Creada clases ms-general

---
+++ front-end/static-files/js/ms-general-spec.js
@@ -0,0 +1,24 @@
+/**
+ * @file ms-Golf-spec.js
+ * @description Fichero TDD para probar todo lo relacionado con MS Golf en el front-end
+ * @author Víctor M. Rivas <vrivas@ujaen.es>
+ * @date 03-feb-2023
+ */
+
+
+
+/*
+IMPORTANTE
+==========
+
+Las pruebas TDD que se encargan de probar las conexiones con el microservicio desde el cliente son difíciles de probar 
+dado que requieren solucionar temas de sincronización. 
+Esto afecta a los métodos:
+ - Golf.descargarRuta
+ - Golf.procesarAcercaDe
+ - Golf.procesarHome
+
+ Las soluciones propuestas en distintos sitios web no han producido el resultado esperado, 
+ por tanto: para esta práctica, se pueden dejar SIN HACER.
+
+ */
